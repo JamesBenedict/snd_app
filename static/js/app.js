a40aa011a85bf6aded6143f869dffabad9b01419
James Benedict <jabenedi@indiana.edu>
2018-01-03 23:32:37 -0500

    Add files via upload

---
+++ static/js/app.js
@@ -0,0 +1,17 @@
+// $('.grid').isotope({
+
+// 	layoutMode: 'packery',
+// 	packery: {
+// 		gutter: '.gutter-sizer'
+// 	},
+// 	itemSelector: '.mini-item',
+// 	percentPosition: true
+// })
+
+
+$('.grid').isotope({
+  layoutMode: 'packery',
+  itemSelector: '.grid-item'
+});
+
+
