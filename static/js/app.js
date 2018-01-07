8d613c9a35e041e23d95b8c6a89c6d61d51d8aff
James Benedict <jabenedi@indiana.edu>
2018-01-07 17:31:32 -0500

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -26,6 +26,40 @@ $('.button-group').each( function( i, buttonGroup ) {
   });
 });
 
+// filter functions
+var filterFns = {
+  // show if name ends with -ium
+  Times: function() {
+    var name = $(this).find('.name').text();
+    return name.match( /New York Times$/ );
+  },
+  Parool: function() {
+    var name = $(this).find('.name').text();
+    return name.match(  /Parool$/ );
+  }
+};
+// https://codepen.io/desandro/pen/wfaGu
+
+
+
+// bind filter button click
+$('.filters-button-group').on( 'click', 'button', function() {
+  var filterValue = $( this ).attr('data-filter');
+  // use filterFn if matches value
+  filterValue = filterFns[ filterValue ] || filterValue;
+  $grid.isotope({ filter: filterValue });
+});
+
+
+
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'button', function() {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $( this ).addClass('is-checked');
+  });
+});
 
 $('#button-top-scroll').click(function(){
   // document.body.scrollTop = document.documentElement.scrollTop = 0;
