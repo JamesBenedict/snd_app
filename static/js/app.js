ad1681aed6d8740719d4a8318219842e8fd677a9
James Benedict <jabenedi@indiana.edu>
2018-01-09 12:33:19 -0500

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -1,23 +1,32 @@
-// var $grid = $('.grid').isotope({
-//   itemSelector: '.grid-item',
-//   layoutMode: 'packery',  
-// });
+// quick search regex
+var qsRegex;
+var buttonFilter;
+
 // init Isotope
 var $grid = $('.grid').isotope({
   itemSelector: '.grid-item',
   layoutMode: 'packery',
-  getSortData: {
-    ref: '.ref parseInt',
+  filter: function() {
+    var $this = $(this);
+    var searchResult = qsRegex ? $this.text().match( qsRegex ) : true;
+    var buttonResult = buttonFilter ? $this.is( buttonFilter ) : true;
+    return searchResult && buttonResult;
   }
 });
 
-// bind sort button click
-$('.sort-by-button-group').on( 'click', 'button', function() {
-  var sortValue = $(this).attr('data-sort-value');
-  $grid.isotope({ sortBy: sortValue });
+$('#filters').on( 'click', 'button', function() {
+  buttonFilter = $( this ).attr('data-filter');
+  $grid.isotope();
 });
 
-// change is-checked class on buttons
+// use value of search field to filter
+var $quicksearch = $('#quicksearch').keyup( debounce( function() {
+  qsRegex = new RegExp( $quicksearch.val(), 'gi' );
+  $grid.isotope();
+}) );
+
+
+  // change is-checked class on buttons
 $('.button-group').each( function( i, buttonGroup ) {
   var $buttonGroup = $( buttonGroup );
   $buttonGroup.on( 'click', 'button', function() {
@@ -25,45 +34,26 @@ $('.button-group').each( function( i, buttonGroup ) {
     $( this ).addClass('is-checked');
   });
 });
+  
 
-// filter functions
-var filterFns = {
-  // show if name ends with -ium
-  Times: function() {
-    var name = $(this).find('.name').text();
-    return name.match( /New York Times$/ );
-  },
-  Parool: function() {
-    var name = $(this).find('.name').text();
-    return name.match(  /Parool$/ );
-  }
-};
-// https://codepen.io/desandro/pen/wfaGu
-
-
-
-// bind filter button click
-$('.filters-button-group').on( 'click', 'button', function() {
-  var filterValue = $( this ).attr('data-filter');
-  // use filterFn if matches value
-  filterValue = filterFns[ filterValue ] || filterValue;
-  $grid.isotope({ filter: filterValue });
-});
+// debounce so filtering doesn't happen every millisecond
+function debounce( fn, threshold ) {
+  var timeout;
+  return function debounced() {
+    if ( timeout ) {
+      clearTimeout( timeout );
+    }
+    function delayed() {
+      fn();
+      timeout = null;
+    }
+    setTimeout( delayed, threshold || 100 );
+  };
+}
 
 
-
-// change is-checked class on buttons
-$('.button-group').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $buttonGroup.on( 'click', 'button', function() {
-    $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $( this ).addClass('is-checked');
-  });
-});
 
 $('#button-top-scroll').click(function(){
-  // document.body.scrollTop = document.documentElement.scrollTop = 0;
-  // $('html').slideUp();
 
   $("html, body").animate({ scrollTop: 0 }, "slow");
 
