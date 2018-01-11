d3d5d1453bbc3bff7973d9781c2ae3b8ce108508
James Benedict <jabenedi@indiana.edu>
2018-01-11 14:33:24 -0500

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -1,36 +1,73 @@
-// quick search regex
+// Right now the search returns results if it matches any entries' text.
+// But I want it to search only a specific field in the entry.
+// Because if I'm searching for gold awards
+// but one of the designer's last name is goldberg
+// then it will show entries with a gold award and those designed by goldberg.
+// Or for the test code I have here, if I search for "World's Best Designed"
+// then all entries are displayed
+// when I only want entries with that in the title 
+// The rest of my questions/explanations are in the comments below
+// here's an example I used to build this if it help https://codepen.io/desandro/pen/mCdbD
+
+// globals
 var qsRegex;
 var buttonFilter;
 var searchField;
-var $container;
-var filters = {};
 
-
-// init Isotope
+// init Isotope https://isotope.metafizzy.co/
 var $grid = $('.grid').isotope({
   itemSelector: '.grid-item',
   layoutMode: 'packery',
-
+  // This is where I start to get lost
   filter: function() {
+    // "This" is the grid item AKA the entry
     var $this = $(this);
-    var $searchField = $('.name_search');
+    // The console log below prints all the grid item's text in a seperate box
+    // EX: "World's Best Designed XX" (next box) "Silver Test phrase with World's Best Designed in it" (next box) ...
+    // console.log($this.text());
+
+    // This was added by me trying to limit the search to the text within a specific class
+    var $searchField = $('.award_search');
+    // The console Log below prints every entries' text contained in the .award_search class in one box
+    // EX: "World's Best DesignedSilverAward of ExcellenceWorld's Best DesignedGold"
+    // console.log($searchField.text())
+
+    // Below is the search that is too inclusive but works.
+    // I think this is what the code means
+    // If the search field's text (qsRegex) matches any text in the grid item ($this) return that item
+    // else return true (IDK what that means but it still seems to work if I change it to false?) 
     var searchResult = qsRegex ? $this.text().match( qsRegex ) : true;
-    // var buttonResult = buttonFilter ? $this.is( buttonFilter ) : true;
-    return searchResult;
+    
+    // Below is what I thought would cause the search to only search the .award_search text
+    // But it returns every grid item as long as the searched term is in any of the .award_search text
+    // EX: so searching "Gold", "Silver", ... returns every result but searching "XX" returns nothing 
+    // I think its searching the string that is printed in the $searchField console log, which is: "World's Best DesignedSilverAward of ExcellenceWorld's Best DesignedGold"
+    // So if I could break up the string and limit it to a specific grid item's .award_search text
+    // then this might work, but IDK how to do limit the $searchField to one grid item
+    // var searchResult = qsRegex ? $searchField.text().match( qsRegex ) : true;
+
+    // This assigns the results of the filter button, everything works here
+    var buttonResult = buttonFilter ? $this.is( buttonFilter ) : true;
+    return searchResult && buttonResult;
   }
 });
 
+// checks for which filter box is selected and lays the grid out depending on the data-filter
 $('#filters').on( 'click', 'button', function() {
   buttonFilter = $( this ).attr('data-filter');
   $grid.isotope();
 });
 
+// uses the value of the search field to filter
 var $quicksearch = $('#quicksearch').keyup( debounce( function() {
+  // creates a search string from the search field 
+  // g: returns every results, i: ignores case
   qsRegex = new RegExp( $quicksearch.val(), 'gi' );
+  // Calls the grid to layout again with the new search
   $grid.isotope();
 }) );
 
-  // change is-checked class on buttons
+// change is-checked class on buttons
 $('.button-group').each( function( i, buttonGroup ) {
   var $buttonGroup = $( buttonGroup );
   $buttonGroup.on( 'click', 'button', function() {
@@ -54,11 +91,14 @@ function debounce( fn, threshold ) {
   };
 }
 
+// The following two functions only will show/matter when I have all the entries loaded
+// scrolls screen back to top 
 $('#button-top-scroll').click(function(){
   $("html, body").animate({ scrollTop: 0 }, "slow");
   console.log('test scroll tops')
 })
 
+// hides button until down the page a bit
 $(function(){
     $(window).scroll(function() { 
         if ($(this).scrollTop() > 500) { 
@@ -71,138 +111,11 @@ $(function(){
     });
 });
 
-
-// ignore this, im working on this next
-// var $container;
-// var filters = {};
-
-// $(function(){
-//   $container = $('.grid');
-//   var $filterDisplay = $('#filter-display');
-//   $container.isotope();
-//   // do stuff when checkbox change
-//   $('#options').on( 'change', function( jQEvent ) {
-//     var $checkbox = $( jQEvent.target );
-//     manageCheckbox( $checkbox );
-//     var comboFilter = getComboFilter( filters );
-//     $container.isotope({ filter: comboFilter });
-//     $filterDisplay.text( comboFilter );
-//   });
-// });
-
-// var $grid = $('.grid').isotope({
-//   itemSelector: '.grid-item',
-//   layoutMode: 'packery',
-
-// // original code 
-//   // filter: function() {
-//   //   var $this = $(this);
-//   //   var searchResult = qsRegex ? $this.text().match( qsRegex ) : true;
-//   //   var buttonResult = buttonFilter ? $this.is( buttonFilter ) : true;
-//   //   return searchResult && buttonResult;
-//   // }
-
-//   filter: function() {
-//     var $this = $(this);
-//     var $searchField = $('.name_search');
-//     var searchResult = qsRegex ? $this.text().match( qsRegex ) : true;
-//     // var buttonResult = buttonFilter ? $this.is( buttonFilter ) : true;
-//     return searchResult;
-//   }
-// });
-
-
-
-
-
-
-
-// function getComboFilter( filters ) {
-//   var i = 0;
-//   var comboFilters = [];
-//   var message = [];
-
-//   for ( var prop in filters ) {
-//     message.push( filters[ prop ].join(' ') );
-//     var filterGroup = filters[ prop ];
-//     // skip to next filter group if it doesn't have any values
-//     if ( !filterGroup.length ) {
-//       continue;
-//     }
-//     if ( i === 0 ) {
-//       // copy to new array
-//       comboFilters = filterGroup.slice(0);
-//     } else {
-//       var filterSelectors = [];
-//       // copy to fresh array
-//       var groupCombo = comboFilters.slice(0); // [ A, B ]
-//       // merge filter Groups
-//       for (var k=0, len3 = filterGroup.length; k < len3; k++) {
-//         for (var j=0, len2 = groupCombo.length; j < len2; j++) {
-//           filterSelectors.push( groupCombo[j] + filterGroup[k] ); // [ 1, 2 ]
-//         }
-
-//       }
-//       // apply filter selectors to combo filters for next group
-//       comboFilters = filterSelectors;
-//     }
-//     i++;
-//   }
-
-//   var comboFilter = comboFilters.join(', ');
-//   return comboFilter;
-// }
-
-// function manageCheckbox( $checkbox ) {
-//   var checkbox = $checkbox[0];
-
-//   var group = $checkbox.parents('.option-set').attr('data-group');
-//   // create array for filter group, if not there yet
-//   var filterGroup = filters[ group ];
-//   if ( !filterGroup ) {
-//     filterGroup = filters[ group ] = [];
-//   }
-
-//   var isAll = $checkbox.hasClass('all');
-//   // reset filter group if the all box was checked
-//   if ( isAll ) {
-//     delete filters[ group ];
-//     if ( !checkbox.checked ) {
-//       checkbox.checked = 'checked';
-//     }
-//   }
-//   // index of
-//   var index = $.inArray( checkbox.value, filterGroup );
-
-//   if ( checkbox.checked ) {
-//     var selector = isAll ? 'input' : 'input.all';
-//     $checkbox.siblings( selector ).removeAttr('checked');
-
-
-//     if ( !isAll && index === -1 ) {
-//       // add filter to group
-//       filters[ group ].push( checkbox.value );
-//     }
-
-//   } else if ( !isAll ) {
-//     // remove filter from group
-//     filters[ group ].splice( index, 1 );
-//     // if unchecked the last box, check the all
-//     if ( !$checkbox.siblings('[checked]').length ) {
-//       $checkbox.siblings('input.all').attr('checked', 'checked');
-//     }
-//   }
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
+// https://stackoverflow.com/questions/23716866/isotope-combining-filter-by-keyword-and-search
+// http://fiddle.jshell.net/lucaspedroza/7t8mgont/
+// https://codepen.io/desandro/pen/JEojz
+// https://codepen.io/desandro/pen/btFfG
+// https://codepen.io/desandro/pen/mCdbD
+// http://everyonelovessharing.com/misc/isotopedemo/
+// http://everyonelovessharing.com/misc/isotope.combofilter.js
+// https://codepen.io/desandro/pen/mCdbD
