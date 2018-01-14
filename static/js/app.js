be4b33f25d04ec4b1431b1e69b827642a35f71e3
James Benedict <jabenedi@indiana.edu>
2018-01-13 19:21:45 -0500

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,3 @@
-// Right now the search returns results if it matches any entries' text.
-// But I want it to search only a specific field in the entry.
-// Because if I'm searching for gold awards
-// but one of the designer's last name is goldberg
-// then it will show entries with a gold award and those designed by goldberg.
-// Or for the test code I have here, if I search for "World's Best Designed"
-// then all entries are displayed
-// when I only want entries with that in the title 
-// The rest of my questions/explanations are in the comments below
-// here's an example I used to build this if it help https://codepen.io/desandro/pen/mCdbD
-
 // globals
 var qsRegex;
 var buttonFilter;
@@ -21,26 +10,18 @@ var $grid = $('.grid').isotope({
   layoutMode: 'packery',
   filter: function() {
     // console.log(searchType)
-
     if (searchType == "awardSearch"){
       var $searchField = $('.award_search', this);
     } else if (searchType == "categorysearch") {
       var $searchField = $('.category_search', this);
     } else if (searchType == "publicationsearch"){
       var $searchField = $('.publication_search', this);
-    } else if (searchField = "designersearch"){
+    } else if (searchType = "designersearch"){
       var $searchField = $('.designer_search', this);
-    }
+    } 
 
-    
-    
-    // The console log below prints all the grid item's text in a seperate box
-    // EX: "World's Best Designed XX" (next box) "Silver Test phrase with World's Best Designed in it" (next box) ...
-    // console.log($this.text());
-
-   var searchResult = qsRegex ? $searchField.text().match( qsRegex ) : true;
-  
-    // This assigns the results of the filter button, everything works here
+    $this = $(this);
+    var searchResult = qsRegex ? $searchField.text().match( qsRegex ) : true;
     var buttonResult = buttonFilter ? $this.is( buttonFilter ) : true;
     return searchResult && buttonResult;
   }
@@ -49,47 +30,81 @@ var $grid = $('.grid').isotope({
 // checks for which filter box is selected and lays the grid out depending on the data-filter
 $('#filters').on( 'click', 'button', function() {
   buttonFilter = $( this ).attr('data-filter');
+
+  document.getElementById('categorysearch').value = '';
+  document.getElementById('awardsearch').value = '';
+  document.getElementById('publicationsearch').value = '';
+  document.getElementById('designersearch').value = '';
+  document.getElementById('categorysearch').placeholder = 'Search Awards';
+  document.getElementById('categorysearch').placeholder = 'Search Categories';
+  document.getElementById('publicationsearch').placeholder = 'Search Publications';
+  document.getElementById('designersearch').placeholder = 'Search Designers';
+  searchField = " ";  
   $grid.isotope();
+  return searchField;
 });
 
 // uses the value of the search field to filter
 var $awardsearch = $('#awardsearch').keyup( debounce( function() {
-  // creates a search string from the search field 
-  // g: returns every results, i: ignores case
   qsRegex = new RegExp( $awardsearch.val(), 'gi' );
   searchType = "awardSearch";
-  // Calls the grid to layout again with the new search
+  buttonFilter = "*";
+
+  document.getElementById('categorysearch').value = '';
+  document.getElementById('publicationsearch').value = '';
+  document.getElementById('designersearch').value = '';
+  document.getElementById('categorysearch').placeholder = 'Search Categories';
+  document.getElementById('publicationsearch').placeholder = 'Search Publications';
+  document.getElementById('designersearch').placeholder = 'Search Designers';
   $grid.isotope();
   return searchType;
 }) );
 
 var $categorysearch = $('#categorysearch').keyup( debounce( function() {
-  // creates a search string from the search field 
   qsRegex = new RegExp( $categorysearch.val(), 'gi' );
   searchType = "categorysearch";
-  // Calls the grid to layout again with the new search
+  buttonFilter = "*";
+
+  document.getElementById('awardsearch').value = '';
+  document.getElementById('publicationsearch').value = '';
+  document.getElementById('designersearch').value = '';
+  document.getElementById('awardsearch').placeholder = 'Search Awards';
+  document.getElementById('publicationsearch').placeholder = 'Search Publications';
+  document.getElementById('designersearch').placeholder = 'Search Designers';
+
   $grid.isotope();
   return searchType;
 }) );
 
 var $publicationsearch = $('#publicationsearch').keyup( debounce( function() {
-  // creates a search string from the search field 
   qsRegex = new RegExp( $publicationsearch.val(), 'gi' );
   searchType = "publicationsearch";
-  // Calls the grid to layout again with the new search
+  buttonFilter = "*";
+
+  document.getElementById('awardsearch').value = '';
+  document.getElementById('categorysearch').value = '';
+  document.getElementById('designersearch').value = '';
+  document.getElementById('awardsearch').placeholder = 'Search Awards';
+  document.getElementById('categorysearch').placeholder = 'Search Categories';
+  document.getElementById('designersearch').placeholder = 'Search Designers';
   $grid.isotope();
   return searchType;
 }) );
 
 var $designersearch = $('#designersearch').keyup( debounce( function() {
-  // creates a search string from the search field 
   qsRegex = new RegExp( $designersearch.val(), 'gi' );
   searchType = "designersearch";
-  // Calls the grid to layout again with the new search
+  buttonFilter = "*";
+
+  document.getElementById('awardsearch').value = '';
+  document.getElementById('publicationsearch').value = '';
+  document.getElementById('categorysearch').value = '';
+  document.getElementById('awardsearch').placeholder = 'Search Awards';
+  document.getElementById('categorysearch').placeholder = 'Search Categories';
+  document.getElementById('publicationsearch').placeholder = 'Search Publications';
   $grid.isotope();
   return searchType;
 }) );
-
 
 // change is-checked class on buttons
 $('.button-group').each( function( i, buttonGroup ) {
@@ -115,7 +130,6 @@ function debounce( fn, threshold ) {
   };
 }
 
-// The following two functions only will show/matter when I have all the entries loaded
 // scrolls screen back to top 
 $('#button-top-scroll').click(function(){
   $("html, body").animate({ scrollTop: 0 }, "slow");
@@ -125,7 +139,7 @@ $('#button-top-scroll').click(function(){
 // hides button until down the page a bit
 $(function(){
     $(window).scroll(function() { 
-        if ($(this).scrollTop() > 1500) { 
+        if ($(this).scrollTop() > 1700) { 
             $("#button-top-scroll:hidden").css('visibility','visible');   
             $("#button-top-scroll:hidden").fadeIn('slow');  
         } 
